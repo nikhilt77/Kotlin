3db05fc2582e3659a20fcb60b39f69ba5353c67e
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-07-02 10:48:56 +0530

    Update Basic_Member_Extension.kt

--- Basic_Member_Extension.kt
+++ Basic_Member_Extension.kt
@@ -18,10 +18,19 @@ fun A.greet() = "Hello, my name is $name and I am $age years old."
 
 fun main() {
     val a = A()
+    
     println(a.foo(2))
     println(a.foo())
     println(a.displayInfo())
-    a.setInfo("Abc", 25)
+    
+    println("Enter new name:")
+    val newName = readLine() ?: "Unknown"
+    
+    println("Enter new age:")
+    val newAge = readLine()?.toIntOrNull() ?: 0
+    
+    a.setInfo(newName, newAge)
+    
     println(a.displayInfo())
     println(a.greet())
 }
